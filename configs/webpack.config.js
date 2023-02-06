746c11886b94ea644d376c53ff1bfdd2f670572b
genesis-0000 <thaibao@autonomous.nyc>
2023-02-06 14:44:19 +0700

    - update

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -4,11 +4,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     mode: 'development',
-    entry: "./src/index.js",
+    entry: /\.js$/i,
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "bundle.js",
-        clean: true
     },
     module: {
         rules: [
